b0fe87984bb943106266257b958d0a09eafc5642
Rasunon-Soare <rasunonsoare@gmail.com>
2023-06-17 22:38:56 +0630

    AdoDotNet CRUD - Complete

--- DotNetCoreTraining20230617/Program.cs
+++ DotNetCoreTraining20230617/Program.cs
@@ -1,19 +1,22 @@
-﻿using DotNetCoreTraining20230617.DbService.Services;
+﻿using DotNetCoreTraining20230617;
+using DotNetCoreTraining20230617.DbService.Services;
 using DotNetCoreTraining20230617.Models;
 using Microsoft.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
 
-SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
-{
-    DataSource = ".",
-    InitialCatalog = "testdb",
-    UserID = "sa",
-    Password = "sa@123"
-};
+//SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+//{
+//    DataSource = ".",
+//    InitialCatalog = "testdb",
+//    UserID = "sa",
+//    Password = "sa@123"
+//};
 
-AdoDotNetService adoDotNetService = new AdoDotNetService(sqlConnectionStringBuilder);
-var lst = await adoDotNetService.Query<BlogDataModel>("select * from tbl_blog with (nolock) order by blog_id desc");
+//AdoDotNetService adoDotNetService = new AdoDotNetService(sqlConnectionStringBuilder);
+//var lst = await adoDotNetService.Query<BlogDataModel>("select * from tbl_blog with (nolock) order by blog_id desc");
+
+await AdoDotNetExample.RunAsync();
 
 Console.Write("Press any key to continue...");
 Console.ReadKey();
